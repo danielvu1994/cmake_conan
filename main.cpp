6166f0daf4b149b161f51e09dc23ffec30912c6f
DanielVu1994 <vulinhdam1994@gmail.com>
2022-06-22 14:44:30 +0000

    Add cmake example

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+#include "my_math/addition.h"
+#include "my_math/division.h"
+#include "my_print/print_result.h"
+
+using namespace std;
+
+main() {
+	float first_no, second_no, result_add, result_div;
+
+	std::cout << "Enter first number \t";
+	std::cin >> first_no;
+	cout << "Enter second number\t";
+	cin >> second_no;
+
+
+	result_add = addition(first_no, second_no);
+	result_div = division(first_no, second_no);
+
+	print_result("Addition", result_add);
+	print_result("Division", result_div);
+
+	return 0;
+}
