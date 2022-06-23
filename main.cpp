3181a1e074b198bb1ca0607306971858d65b7593
DanielVu1994 <vulinhdam1994@gmail.com>
2022-06-23 04:39:39 +0000

    mos example - Linux added

--- main.cpp
+++ main.cpp
@@ -1,27 +1,64 @@
-#include <iostream>
+#include <stdio.h>
+#include "mosquitto.h"
 
-#include "my_math/addition.h"
-#include "my_math/division.h"
-#include "my_print/print_result.h"
+void my_log_callback(struct mosquitto *mosq, void * obj, int level, const char * msg) {
+	//Ignore level, just print it
+	fprintf(stderr, "level: %d, msg: %s \n", level, msg);
+}
 
-using namespace std;
+void my_connect_callback(struct mosquitto *mosq, void * obj, int rc) {
+	if(rc !=0) {
+		fprintf(stderr, "Connection failed, return code = %d \n", rc);
+	}
 
-int main() {
-	float first_no, second_no, result_add, result_div;
+	fprintf(stderr, "Connection success! Subscribe to test/topic\n");
+	mosquitto_subscribe(mosq, NULL, "test/topic", 0);
+}
 
-	std::cout << "Enter first number \t";
-	std::cin >> first_no;
-	cout << "Enter second number\t";
-	cin >> second_no;
+void my_message_callback(struct mosquitto *mosq, void *obj, const struct mosquitto_message *msg) {
+       	fprintf(stderr, "New message with topic %s: %s\n", msg->topic, (char *) msg->payload);
+}
+
+void my_publish_callback (struct mosquitto * mosq, void * obj, int mid) {
+	fprintf(stderr, "message id %d\n", mid);
+}
 
 
-	result_add = addition(first_no, second_no);
-	result_div = division(first_no, second_no);
 
-	print_result("Addition", result_add);
-	print_result("Division", result_div);
+int main(int argc, char *argv[])
+{
+	int i;
+	char host[64];
+	int port = 1883;
+	int keepalive = 60;
+	bool clean_session = true;
+	struct mosquitto *mosq = NULL;
+	char mqMsg[64] = {0,};
 
-	system("pause");
+	sprintf(host, "broker.hivemq.com");
 
+	mosquitto_lib_init();
+	mosq = mosquitto_new(NULL, clean_session, NULL);
+
+	if(!mosq){
+		fprintf(stderr, "Error: Out of memory.\n");
+		return 1;
+	}
+
+	mosquitto_log_callback_set(mosq, my_log_callback);
+	mosquitto_connect_callback_set(mosq, my_connect_callback);
+	mosquitto_message_callback_set(mosq, my_message_callback);
+	mosquitto_publish_callback_set(mosq, my_publish_callback);
+
+	if(mosquitto_connect(mosq, host, port, keepalive)){
+		fprintf(stderr, "Unable to connect.\n");
+		return 1;
+	}
+
+
+        mosquitto_loop_forever(mosq, -1, 1);
+
+	mosquitto_destroy(mosq);
+	mosquitto_lib_cleanup();
 	return 0;
 }
